fff8d242bde7d107610fbff5692e573257b712e8
ishaan-shah52 <102432171+ishaan-shah52@users.noreply.github.com>
2025-10-02 14:01:14 -0400

    fixed up step 2 to have session id for later training

--- s2combine_all_sessions.py
+++ s2combine_all_sessions.py
@@ -4,18 +4,18 @@ import glob, os
 #grab all csv files with data
 #glob library uses wildcard naming convention with *
 file_pattern = "eeg_sessions/eeg_action_data_*.csv"
-file_list = sorted(glob.glob(file_pattern))
+file_list = sorted(glob.glob(file_pattern)) #unnecessary to sort
 print("Found files:", file_list)
 
 eachEEG = []
 
 for idx, file in enumerate(file_list):
     EEG_file = pd.read_csv(file, header=0)
-    eachEEG.append(EEG_file)
+    eachEEG.append(EEG_file) #array of all files
 
 if not eachEEG:
     raise ValueError("No files found.")
 
-allEEG = pd.concat(eachEEG, ignore_index=True)
+allEEG = pd.concat(eachEEG, ignore_index=True) #puts all files tg
 allEEG.to_csv("combined_eeg_data.csv", index=False)
 print("Combined -> combined_eeg_data.csv")
