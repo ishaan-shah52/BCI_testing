345dba9d1ca77e358ffd25b86f15e14da9cae1b0
ishaan-shah52 <102432171+ishaan-shah52@users.noreply.github.com>
2025-10-21 19:45:03 -0400

    updated notes on step0

--- s0EEG_board_mac_finder.py
+++ s0EEG_board_mac_finder.py
@@ -1,6 +1,20 @@
+"""
+Notes:
+A bleak scan goes through Bluetooth Low Energy devices like the OpenBCI ganglion
+-Wraps the window bluetooth APIs to scan for BLE devices and connect by its address
+-Reads and writes data points and stream EEG samples
+
+Bluetooth Low Energy device is a power efficient version of Bluetooth
+RSSI is returned which is the signal strength
+MAC address is a unique hardware identifier
+UUID is a unique identifier in software to find objects or devices
+Persisting means to save information for later use
+"""
+
 from bleak import BleakScanner
 import asyncio
 
+#runs for 5 seconds
 async def main():
     devices = await BleakScanner.discover()
     for device in devices:
